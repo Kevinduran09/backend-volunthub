9893e73970c7ccc0e8248d79d9a80eaf06226782
Kevin Andrey Durán Martínez <kevinduran.net.123@gmail.com>
2025-06-01 05:29:14 -0600

    cambios

--- src/graphql/event/eventResolver.js
+++ src/graphql/event/eventResolver.js
@@ -2,19 +2,36 @@ import * as eventService from "../../services/eventService.js";
 
 export const eventResolver = {
   Query: {
-    eventos: () => {
-      const events = eventService.getEvents()
-      return events
+    eventos: (root, { busqueda }) => {
+      const events = eventService.getEvents(busqueda);
+      return events;
+    },
+    eventosCercanos: (root, { lat, lon, radio }) => {
+      const events = eventService.getEventosCercanos(lat, lon, radio);
+      return events;
     },
     evento: (root, { id }) => eventService.getEventById(id),
   },
-  Evento:{
-    ubicacion:(root)=>{
-      return{
-        latitud:root.latitud,
-        longitud:root.longitud
-      }
-    }
-  }
-  
+  Evento: {
+    ubicacion: (root) => {
+      return {
+        latitud: root.latitud,
+        longitud: root.longitud,
+      };
+    },
+    participantes: (root) => {
+      const participantes = eventService.getParticipantes(root.id);
+      return participantes;
+    },
+    participantes_inscritos: (root) => {
+      const participantes_inscritos = eventService.getCantdadInscritos(root.id);
+      return participantes_inscritos;
+    },
+  },
+  Mutation: {
+    createEvent: async (root, { input }) => {
+      const evento = await eventService.createEvent(input);
+      return evento;
+    },
+  },
 };
--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,7 +1,9 @@
 import { supabase } from "../config/supabaseClient.js";
 
-export async function getEvents() {
-  const { data, error } = await supabase.rpc('get_eventos_latlng')
+export async function getEvents(busqueda) {
+  const { data, error } = await supabase.rpc("get_eventos", {
+    search_term: busqueda,
+  });
   if (error) return new Error(error.message);
   return data;
 }
@@ -16,5 +18,68 @@ export async function getEventById(idEvent) {
 
   return data;
 }
+export async function getEventosCercanos(lat, lon, radio = 5000) {
+  const { data, error } = await supabase.rpc("get_events_nearby", {
+    p_lon: lon,
+    p_lat: lat,
+    p_radius_m: radio,
+  });
+  console.log(error);
 
-export async function name(params) {}
+  if (error) return new Error(error.message);
+
+  return data;
+}
+export async function getParticipantes(idEvent) {
+  const { data, error } = await supabase
+    .from("inscripciones")
+    .select("*")
+    .eq("id_evento", idEvent);
+  if (error) return new Error(error.message);
+  return data;
+}
+export async function searchEventsByTitle(searchTerm) {
+  const { data, error } = await supabase.rpc("get_eventos", {
+    search_term: searchTerm,
+  });
+
+  if (error) return new Error(error.message);
+  return data;
+}
+
+export async function getCantdadInscritos(idEvent) {
+  const { count, error } = await supabase
+    .from("inscripciones")
+    .select("id_evento", { count: "exact", head: true })
+    .eq("id_evento", idEvent);
+  if (error) return new Error(error.message);
+  return count;
+}
+export async function createEvent(input) {
+  const { data, error } = await supabase
+    .from("eventos")
+    .insert([
+      {
+        titulo: input.titulo,
+        descripcion: input.descripcion,
+        fecha: input.fecha,
+        hora_inicio: input.hora_inicio,
+        zona: input.zona,
+        direccion: input.direccion,
+        ubicacion: `SRID=4326;POINT(${input.ubicacion.longitud} ${input.ubicacion.latitud})`,
+        requiere_voluntarios: input.requiere_voluntarios,
+        image_url: input.image_url ?? "",
+        cantidad_participantes_requeridos:
+          input.cantidad_participantes_requeridos,
+        categoria: 1,
+        materiales_requeridos: input.materiales_requeridos ?? "",
+        habilidades_requeridas: input.habilidades_opcionales ?? "",
+        requisitos_adicionales: input.habilidades_requeridas ?? "",
+      },
+    ])
+    .select("id,titulo,descripcion,fecha,hora_inicio,zona,requiere_voluntarios")
+    .single();
+
+  if (error) return new Error(error.message);
+  return data;
+}
